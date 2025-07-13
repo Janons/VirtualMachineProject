5da56880d63c049148139dd95b3a1f712a5f0dfb
Janons <candogrull@hotmail.com>
2025-07-13 14:07:22 +0200

    Created a library, made changes to the main file

---
+++ docs/src/opFunctions.h
@@ -0,0 +1,25 @@
+#ifndef OPFUNCTIONS_H
+#define OPFUNCTIONS_H
+#include <cstdint>
+
+// all the functions to be used for the opcodes
+
+/*The ADD instruction takes two numbers, adds them together, and stores the result in a register*/
+void ADD(uint16_t *reg, uint16_t instr);
+
+/**/
+void LDI();
+/**/
+void AND();
+/**/
+void NOT();
+/**/
+void BR();
+/**/
+void JMP();
+/**/
+void LD();
+/**/
+void STR();
+
+#endif
