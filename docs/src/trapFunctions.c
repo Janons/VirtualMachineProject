5c0eb9f1284ee58a49a145bbacda7ea1b9e327eb
Janons <candogrull@hotmail.com>
2025-07-20 16:21:15 +0200

    Added more trap functions

--- docs/src/trapFunctions.c
+++ docs/src/trapFunctions.c
@@ -14,3 +14,42 @@ void PUTS(uint16_t memory, uint16_t *reg)
 
     fflush(stdout);
 }
+
+void OUT(uint16_t *reg)
+{
+    uint16_t *c = reg[R_R0];
+
+    while (*c)
+    {
+        putc((char)*c, stdout);
+        ++c;
+    }
+
+    fflush(c);
+}
+
+void PUTSP(uint16_t memory, uint16_t *reg)
+{
+    uint16_t *c = memory + reg[R_R0];
+    while (*c)
+    {
+        char char1 = (*c) & 0xFF;
+        putc(char1, stdout);
+        char char2 = (*c) >> 8;
+        if (char2)
+            putc(char2, stdout);
+        ++c;
+    }
+    fflush(stdout);
+}
+
+uint16_t IN(uint16_t *reg)
+{
+    printf("Enter a character");
+    char c = getchar();
+    putc(c, stdout);
+    fflush(stdout);
+
+    reg[R_R0] = (uint16_t)c;
+    return R_R0;
+}
--- docs/src/trapFunctions.h
+++ docs/src/trapFunctions.h
@@ -4,6 +4,11 @@
 
 #include <stdint.h>
 
-void PUTS(uint16_t memory);
+void PUTS(uint16_t memory, uint16_t *reg);
+void OUT(uint16_t *reg);
+
+void PUTSP(uint16_t memory, uint16_t *reg);
+
+uint16_t IN(uint16_t *reg);
 
 #endif
